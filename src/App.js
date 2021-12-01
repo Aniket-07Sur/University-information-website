b466832be99121827499e608f4b3a07fcf4df861
Aniket <aniketsur1109@gmail.com>
2021-12-01 10:44:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from "react";
+import { Link, BrowserRouter } from "react-router-dom";
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getData();
+  }, [])
+  async function getData() {
+    let result = await fetch('http://universities.hipolabs.com/search?country=India');
+    result = await result.json();
+    setData(result);
+  }
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading-div">
+        <h1 className="heading">Select university from the below list</h1>
+      </div>
+      <table className="table table-striped .ml-3 col-sm-6 offset-sm-3" >
+        <thead>
+          <tr>
+            <th scope="col">No.</th>
+            <th scope="col">Name</th>
+            <th scope="col">Contry</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((item, i) =>
+              <tr key={i}>
+                <td>{i + 1}</td>
+                <a href={item.web_pages[0]} target="_blank">{item.name}</a>
+                <td>{item.country}</td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
-
 export default App;
